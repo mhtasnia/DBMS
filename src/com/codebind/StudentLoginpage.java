9f77b842e955fab4f1a991c3f3a4b95ba9c87b50
Meherose Hossain Tasnia <mhtasnia@gmail.com>
2023-10-28 17:48:57 +0600

    DBMS commit of working code

--- src/com/codebind/StudentLoginpage.java
+++ src/com/codebind/StudentLoginpage.java
@@ -2,11 +2,24 @@ package com.codebind;
 
 import javax.swing.*;
 
-public class StudentLoginpage {
-    public Object setVisible;
+public class StudentLoginpage extends JFrame{
+
+    StudentLoginpage(){
+        this.setContentPane(this.panel1);
+        this.setVisible(true);
+        this.setTitle("Student Login");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setBounds(200, 200, 300, 200);
+    }
     private JPanel panel1;
     private JTextField textField1;
-    private JPasswordField passwordField1;
+    private JTextField textField2;
     private JButton logInButton;
     private JButton signUpButton;
+
+    public static void main(String[] args) {
+        StudentLoginpage f1 = new StudentLoginpage();
+
+    }
+
 }
--- src/com/codebind/Usertypechoose.java
+++ src/com/codebind/Usertypechoose.java
@@ -5,18 +5,35 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Usertypechoose {
+public class Usertypechoose extends JFrame{
     private JPanel panel1;
     private JButton StudentloginButton;
     private JButton AdminloginButton;
     private JButton VerifierloginButton;
+    private JLabel stext;
+    private JLabel atext;
+    private JLabel vtext;
 
 
     public Usertypechoose() {
         StudentloginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                StudentLoginpage f2 = new StudentLoginpage();
+                dispose();
+                f2.setVisible(true);
+
             }
         });
+    }
+
+    public static void main(String[] args) {
+        Usertypechoose frame = new Usertypechoose();
+        frame.setContentPane(frame.panel1);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.setBounds(200, 200, 400, 300);
+        frame.setTitle("Login");
+
     }
 }
