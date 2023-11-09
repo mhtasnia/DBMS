3bef4de76c157a749e3c414b7d28cdc5f0ab32e3
Meherose Hossain Tasnia <mhtasnia@gmail.com>
2023-11-10 02:16:43 +0600

    DBMS commit of working code

--- src/com/codebind/AdminInterface.java
+++ src/com/codebind/AdminInterface.java
@@ -7,7 +7,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Vector;
 import javax.swing.*;
@@ -22,6 +21,12 @@ public class AdminInterface extends JFrame{
     private JComboBox comboBox1;
     private JDateChooser JDateChooser1;
     private JButton viewButton;
+    private JTextArea textArea1;
+    private JTextArea textArea2;
+    private JComboBox comboBox2;
+    private JComboBox RouteBox3;
+    private JComboBox timeBox4;
+    private JComboBox BusBox5;
 
     private void createUIComponents() {
         JDateChooser1 = new JDateChooser();
@@ -59,10 +64,10 @@ public class AdminInterface extends JFrame{
 
 
 
-    private void createTable2( String route, Date date) {
+    private void createTable2(String route, Date date, String time) {
         DefaultTableModel tableModel2 = new DefaultTableModel(
                 null,
-                new String[] {"Student id", "Route", "Date", "Time"}
+                new String[]{"Student id", "Route", "Date", "Time"}
         );
 
         tableModel2.setRowCount(0);
@@ -78,24 +83,32 @@ public class AdminInterface extends JFrame{
             queryBuilder.append(" AND bus_route = ?");
         }
 
+        if (time != null && !time.isEmpty()) {
+            queryBuilder.append(" AND booking_time = ?");
+        }
+
         queryBuilder.append(" ORDER BY bus_route");
 
-        try (PreparedStatement preparedStatement = connection.prepareStatement(queryBuilder.toString())) {
+        try  {
+            PreparedStatement preparedStatement = connection.prepareStatement(queryBuilder.toString());
             int parameterIndex = 1;
 
-            if (date != null ) {
-                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-                String formattedDate = dateFormat.format(date);
-                queryBuilder.append(" AND booking_date = ?");
-                preparedStatement.setString(parameterIndex, formattedDate);
+            if (date != null) {
+                preparedStatement.setDate(parameterIndex, new java.sql.Date(date.getTime()));
                 parameterIndex++;
             }
 
             if (route != null && !route.isEmpty()) {
                 preparedStatement.setString(parameterIndex, route);
+                parameterIndex++;
             }
 
-            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+            if (time != null && !time.isEmpty()) {
+                preparedStatement.setString(parameterIndex, time);
+            }
+
+            try  {
+                ResultSet resultSet = preparedStatement.executeQuery();
                 while (resultSet.next()) {
                     Vector<Object> rowData = new Vector<>();
                     rowData.add(resultSet.getString("student_id"));
@@ -104,20 +117,64 @@ public class AdminInterface extends JFrame{
                     rowData.add(resultSet.getString("booking_time"));
                     tableModel2.addRow(rowData);
                 }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
             }
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
         table2.setModel(tableModel2);
     }
+    private int countFilteredEntries(String route, Date date) {
+        int count = 0;
+        Connection connection = DatabaseConnection.getConnection();
 
+        StringBuilder queryBuilder = new StringBuilder("SELECT COUNT(*) AS entry_count FROM busbookinginfo WHERE 1=1");
+
+        if (date != null) {
+            queryBuilder.append(" AND booking_date = ?");
+        }
+
+        if (route != null && !route.isEmpty()) {
+            queryBuilder.append(" AND bus_route = ?");
+        }
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(queryBuilder.toString())) {
+            int parameterIndex = 1;
+
+            if (date != null) {
+                preparedStatement.setDate(parameterIndex, new java.sql.Date(date.getTime()));
+                parameterIndex++;
+            }
+
+            if (route != null && !route.isEmpty()) {
+                preparedStatement.setString(parameterIndex, route);
+            }
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    count = resultSet.getInt("entry_count");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return count;
+    }
+
+    private int countBus(int count) {
+        int buscount = (count / 30) + ((count % 30 != 0) ? 1 : 0);
+        return buscount;
+    }
 
 
     public AdminInterface(){
         this.setContentPane(this.panel1);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setBounds(200, 50, 600, 400);
+        this.setBounds(200, 50, 700, 450);
         createTable();
         addBusButton.addActionListener(new ActionListener() {
             @Override
@@ -131,12 +188,37 @@ public class AdminInterface extends JFrame{
             public void actionPerformed(ActionEvent e) {
                 String route = (String) comboBox1.getSelectedItem();
                 Date date = JDateChooser1.getDate();
-                createTable2(route, date);
+                String time = (String) comboBox2.getSelectedItem();
+                createTable2(route, date, time);
+                int count = countFilteredEntries(route, date);
+                textArea1.setText (" "+count+" ");
+                int buscount = countBus(count);
+                textArea2.setText(" "+buscount+" ");
             }
         });
     }
 
+    private List<String> fetchDataFromDatabase() {
+        List<String> options = new ArrayList<>();
 
+        try {
+            Connection connection = DatabaseConnection.getConnection();
+            String sql = "SELECT column_name FROM your_table_name";
+            try (Statement statement = connection.createStatement();
+                 ResultSet resultSet = statement.executeQuery(sql)) {
+
+                while (resultSet.next()) {
+                    String option = resultSet.getString("column_name");
+                    options.add(option);
+                }
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return options;
+    }
 
     public static void main(String[] args) {
         AdminInterface frame = new AdminInterface();
